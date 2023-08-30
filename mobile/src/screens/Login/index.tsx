35028768558f0682e7a8a25bf768e5b978af3593
ViniDevBR <vinigtr386@gmail.com>
2023-08-30 19:28:59 -0300

    :fire: FIX BUGS

--- mobile/src/screens/Login/index.tsx
+++ mobile/src/screens/Login/index.tsx
@@ -2,10 +2,8 @@ import { View } from 'react-native'
 import { ScreenProps } from '../../types/navigation'
 
 
-
-
-
-export function Login({ route, navigation: { navigate }}: ScreenProps<'Login'>) {
+export function Login({ route, navigation }: ScreenProps<'Login'>) {
+ 
   
   return (
     <View />
--- mobile/src/types/navigation.d.ts
+++ mobile/src/types/navigation.d.ts
@@ -1,10 +1,13 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
 declare global {
   namespace ReactNavigation {
     interface RootParamList extends RootStackParamList {}
   }
 }
 
-export type ScreenProps<T extends keyof RootStackParamList> = NativeStackScreenProps<RootStackParamList, T>
+export type ScreenProps
+  <T extends keyof RootStackParamList> = NativeStackScreenProps<RootStackParamList, T>
 
 export type RootStackParamList = {
   Home: undefined
