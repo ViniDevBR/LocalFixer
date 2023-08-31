48bf2e0ca0bcba2881a4edac06b5ff1febc19400
ViniDevBR <vinigtr386@gmail.com>
2023-08-31 18:02:59 -0300

    :fire: New colors and libs

--- mobile/src/theme/light.ts
+++ mobile/src/theme/light.ts
@@ -1,7 +1,28 @@
 export default {
   COLORS: {
     BACKGROUND: '#F3F3F3',
+
+    PRIMARY: '#0077FF',
+    SECONDARY: '#00A86B',
+    TERCIARY: '#FD3C4A',
+    
+    GRAY_100: '#D9D9D9',
     GRAY_500: '#6F6F6F',
-    TEXT_1: '#000'
+
+    PLACEHOLDER: '#91919F',
+    
+    TEXT: '#000',
+    TEXT_1: '#F3F3F3'
+  },
+  WEIGHT: {
+    Thin :'100', 
+    ExtraLight :'200', 
+    Light :'300', 
+    Regular :'400', 
+    Medium :'500', 
+    SemiBold :'600', 
+    Bold :'700', 
+    ExtraBold :'800', 
+    Black :'900',
   }
 }
